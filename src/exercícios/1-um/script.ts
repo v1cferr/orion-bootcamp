08b1d43efb55125b59a5ba42800316d6f487d659
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-10-11 22:34:38 -0300

    Alterações realizadas a partir do feedback da PR

--- src/exercícios/1-um/script.ts
+++ src/exercícios/1-um/script.ts
@@ -1,38 +1,43 @@
 /*
-*
-* 1- Criar uma função que retorne a quantidade de vogais da palavra passada.
-* 
-* a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
-* b) Dar um exemplo de uso com uma palavra recebida via input no formulário.
-*
-*/
+ *
+ * 1- Criar uma função que retorne a quantidade de vogais da palavra passada.
+ *
+ * a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
+ * b) Dar um exemplo de uso com uma palavra recebida via input no formulário.
+ *
+ */
 
 // Exercício 1 - a)
 
 // let palavraEscolhida: string = "testeComOutraPalavraBarraFrase";
-let palavraEscolhida: string = "antedeguemon";
+const palavraEscolhida: string = "antedeguemon";
 
-function quantidadeEListaDeVogais(palavra: string): {quantidade: number; vogais: string } {
-        let quantidade = 0;
-        let vogais = "";
-    
-        for (let i = 0; i < palavra.length; i++) {
-            if (
-                palavra[i] == "a" ||
-                palavra[i] == "e" ||
-                palavra[i] == "i" ||
-                palavra[i] == "o" ||
-                palavra[i] == "u"
-            ) {
-                vogais += palavra[i];
-                quantidade++;
-            }
-        }
-        return { quantidade, vogais };
-};
+function quantidadeEListaDeVogais(palavra: string): {
+	quantidade: number;
+	vogais: string;
+} {
+	let quantidade: number = 0;
+	let vogais: string = "";
+
+	for (let i = 0; i < palavra.length; i++) {
+		if (
+			palavra[i] === "a" ||
+			palavra[i] === "e" ||
+			palavra[i] === "i" ||
+			palavra[i] === "o" ||
+			palavra[i] === "u"
+		) {
+			vogais += palavra[i];
+			quantidade++;
+		}
+	}
+	return { quantidade, vogais };
+}
 
 const resultado = quantidadeEListaDeVogais(palavraEscolhida);
-console.log(`A palavra "${palavraEscolhida}" tem ${resultado.quantidade} vogais. Sendo elas, respectivamente: "${resultado.vogais}".`);
+console.log(
+	`A palavra "${palavraEscolhida}" tem ${resultado.quantidade} vogais. Sendo elas, respectivamente: "${resultado.vogais}".`
+);
 
 // Exercício 1 - b)
 
@@ -41,10 +46,10 @@ const button = document.getElementById("button") as HTMLButtonElement;
 const readonly = document.getElementById("readonly") as HTMLTextAreaElement;
 
 button.addEventListener("click", () => {
-    const palavraDigitada = input.value;
-    const resultado = quantidadeEListaDeVogais(palavraDigitada);
+	const palavraDigitada = input.value;
+	const resultado = quantidadeEListaDeVogais(palavraDigitada);
 
-    let resultadoFormatado: string = `A palavra "${palavraDigitada}" tem ${resultado.quantidade} vogais. Sendo elas, respectivamente: "${resultado.vogais}".`;
-    console.log(resultadoFormatado);
-    readonly.value = resultadoFormatado;
+	let resultadoFormatado: string = `A palavra "${palavraDigitada}" tem ${resultado.quantidade} vogais. Sendo elas, respectivamente: "${resultado.vogais}".`;
+	console.log(resultadoFormatado);
+	readonly.value = resultadoFormatado;
 });
